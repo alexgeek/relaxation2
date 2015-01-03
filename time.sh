a1303d8af1d97800becf721ae41bbb37725e6795
Alexander Brook Perry <alex@alexgeek.co.uk>
2015-01-03 19:28:04 +0000

    Testing tweaks

--- time.sh
+++ time.sh
@@ -2,7 +2,7 @@
 START=${1:-1}
 INC=${2:-1}
 FINISH=${3:-8}
-N=${4:-2050}
+N=${4:-514}
 TRIALS=1
 echo "Testing with threading in range $START-$FINISH in increments of $INC for $N by $N array."
 OUTPUT="time.log"
@@ -19,5 +19,19 @@ gnuplot <<- EOF
     #set term dumb
     set term png
     set output "${OUTPUT}.png"
-    plot [$START:$FINISH] "${OUTPUT}" title "Speedup (real)" with lines
+    plot [$START:$FINISH] "${OUTPUT}" title "Execution Time (real)" with lines
+EOF
+
+S=$(head -1 ${OUTPUT} | awk '{print $1}')
+ST=$(grep "^$S[^0-9]" 'time.real.log' | awk '{ sum += $2; n++ } END { if (n > 0) print sum / n; }')
+echo "T(1) = $ST"
+gnuplot <<- EOF
+    set xlabel "Threads"
+    set ylabel "Speedup"
+    set title "Speedup Results"
+    set xrange [$START:$FINISH]
+    #set term dumb
+    set term png
+    set output "speedup.png"
+    plot ${OUTPUT} using 1:($ST/\$2) title "(real)" smooth unique lw 3
 EOF
